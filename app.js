e5ee522a5fb8fc29a2dd2d8650ac510299000c92
germanique <antoine.germanique@campus.afpa.fr>
2016-05-07 14:09:33 +0200

    master

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 // Database
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk(MONGODB_URI);
+var mongoUri = process.env.MONGODB_URI || 'localhost:27017/nodetest2'
+var db = monk(mongoUri);
 //cors
 var cors = require('cors')
 
